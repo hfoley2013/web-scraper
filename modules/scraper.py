d0b5ab3af59f1941123d7a0d9207d3520f996ed3
Harper Foley <harper.e.foley@gmail.com>
2023-01-10 12:14:19 -0800

    Updates README and adds user input to program

--- modules/scraper.py
+++ modules/scraper.py
@@ -1,5 +1,10 @@
 import requests
+import re
 from bs4 import BeautifulSoup
+
+user_input_url = input('> Input the URL you wish to search for citations: ')
+url_match = re.search(r"https://[^ ]+", user_input_url)
+parsed_url = url_match.group(0)
 
 def get_citations_needed_count(url):
     page = requests.get(url)
@@ -8,6 +13,7 @@ def get_citations_needed_count(url):
     citations_needed = soup.find_all("a", title="Wikipedia:Citation needed")
     citation_count = len(citations_needed)
     
+    print(citation_count)
     return citation_count
 
 def get_citations_needed_report(url):
@@ -20,7 +26,11 @@ def get_citations_needed_report(url):
     for element in citations_needed:
         statements_needing_citations += element.parent.get_text() + "\n"
     
+    print(str(statements_needing_citations))
     return str(statements_needing_citations)
+
+get_citations_needed_count(parsed_url)
+get_citations_needed_report(parsed_url)
 
 # Test the function
 if __name__ == '__main__':
